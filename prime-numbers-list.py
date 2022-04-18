f5c37cb89b630d993651882e5bcde78544f2e44d
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-04-18 21:11:23 +0200

    Update prime-numbers-list.py

--- prime-numbers-list.py
+++ prime-numbers-list.py
@@ -12,17 +12,17 @@ def prime():
   try:
     a1=int(a)
   except: #if first input is not an integer
-    print("Integers only! (" + a + " is not an integer!)")
+    print("Integers only! ('" + a + "' is not an integer!)")
     exit()
   try:  
     b1=int(b)
   except: #if second input is not an integer
-    print("Integers only! (" + b + " is not an integer!)")
+    print("Integers only! ('" + b + "' is not an integer!)")
     exit()
-  if b < a: #if second number is larger
+  if b1 < a1: #if second number is smaller
     print ("Second number must be larger than first!")
     exit()
-  elif a == "2" or a == "1": #1 and 2 are not considered as primes so they can be omitted from this test
+  elif a == "2" or a == "1": #1 and 2 are not considered primes so they can be omitted from this test
     a1 = 3
   for p in range(a1, b1, 1): #range of test
     d = round(p / 2) + 1
