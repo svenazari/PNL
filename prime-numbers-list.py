0cc1d92af317bd487cb7b89803cc2fd3e4a25d7c
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-04-18 15:10:47 +0100

    Add files via upload

--- prime-numbers-list.py
+++ prime-numbers-list.py
@@ -9,24 +9,26 @@ def prime():
   a = input("FROM: ")
   b = input("TO: ")
   primel = [] #list to store primes
-  if a == "2" or a == "1":
-    a1 = 3
-  else:
-    try:
-      a1 = int(a)
-    except:
-      print("INPUT ONLY INTEGERS!")
-      exit ()
   try:
-    b1 = int(b) + 1
-  except:
-    print("INPUT ONLY INTEGERS!")
-    exit ()
+    a1=int(a)
+  except: #if first input is not an integer
+    print("Integers only! (" + a + " is not an integer!)")
+    exit()
+  try:  
+    b1=int(b)
+  except: #if second input is not an integer
+    print("Integers only! (" + b + " is not an integer!)")
+    exit()
+  if b < a: #if second number is larger
+    print ("Second number must be larger than first!")
+    exit()
+  elif a == "2" or a == "1": #1 and 2 are not counted as primes so they can be omitted from this test
+    a1 = 3
   for p in range(a1, b1, 1): #range of test
     d = round(p / 2) + 1
     for d in range(2, d, 1): #range of division
-      test = p % d
-      if test == 0:
+      test = p % d #test for remainder
+      if test == 0: #if there is reminder, number is not prime
         break
     if test != 0:
       primel.append(p) #if number is prime, append it to list
@@ -37,4 +39,4 @@ def prime():
   print("PRIMES:")
   print(primel) #print primes
 
-prime()
+prime() #call function
